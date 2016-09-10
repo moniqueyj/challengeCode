aeb97afea12c1986b49cae2d2ea0c730770349f1
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-09-10 16:04:33 -0700

    still working

--- app/component/create-data-table/index.js
+++ app/component/create-data-table/index.js
@@ -5,30 +5,21 @@ const sampleApp = angular.module('sampleApp');
 
 sampleApp.component('createDataTable',{
   template: require('./create-data-table.html'),
-  controller: ['$scope', '$http', 'NgTableParams', function CreateTableController($scope, $http, NgTableParams){
-    $scope.listTable = new NgTableParams(
-      {count: 5},
-      {
-        getData: function (params){
-          var count = params.count();
-          var page = params.page();
-          var filter = params.filter();
-          console.log('count', filter);
-          console.log('page', page);
-          console.log('count', count);
-          console.log('aruguments', arguments );
-          let url = 'https://gist.githubusercontent.com/evanjacobs/c150c0375030dc4de65e9b95784dc894/raw/35c5f455b147703db3989df0cb90f5781c3b312f/usage_data.json';
-          return $http.get(url).then(res => {
-            params.total(res.data.length);
-            let data = res.data;
-            data = data.filter(value => {
-              if (value.users < 100) return true;
-              return false;
-            });
-            return data.slice(page * count, page * count + count);
-          });
-        }
-      }
-    );
-  }]
+  controller: 'CreateTableController',
+  controllerAs:'creatteTableCtrl'
 });
+
+sampleApp.controller('CreateTableController', ['$scope', '$http', CreateTableController]);
+function CreateTableController($scope, $http){
+  $http.get('https://gist.githubusercontent.com/evanjacobs/c150c0375030dc4de65e9b95784dc894/raw/35c5f455b147703db3989df0cb90f5781c3b312f/usage_data.json')
+  .then(function(response){
+    response.data.sort(function(a, b) {
+      return new Date(b.date) - new Date(a.date);
+    });
+    response.data[0].date;
+    $scope.list = response.data;
+  });
+  $scope.byMonth = function (){
+    value: new Date(2016,8,24);
+  };
+}
--- app/entry.js
+++ app/entry.js
@@ -4,12 +4,11 @@
 require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
-const ngTable = require('ng-table');
 
 // npm modules
 const angular = require('angular');
 
 // angular modules
-angular.module('sampleApp',['ngTable']);
+angular.module('sampleApp',[]);
 require('./component/create-data-table');
 // require('./component/table-list-data-chart');
